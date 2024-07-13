60dc0797f3d4941bc4a3cc189d9f51626d7bb175
Gogas1 <kargnikita5@gmail.com>
2024-07-13 14:56:21 +0200

    New home page base

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,53 +1,19 @@
-import { Link } from 'react-router-dom'
 import '../styles/pages/home/home.scss'
-import instaIcon from '../assets/icons/home/insta_idle.svg';
-import ttIcon from '../assets/icons/home/tt_idle.svg';
-import fbIcon from '../assets/icons/home/fb_idle.svg';
-import destIcon from '../assets/icons/home/dest_icn.svg';
-import TextField from './home/TextField';
 import { useTranslation } from 'react-i18next';
 
 export default function Home() {
     const { t, i18n } = useTranslation();
+    document.title = `Mandry - ${t('homeTitle')}`
 
     return (
         <>
             <div id="home-page">
-                <div id='side-line'>
-                    <div className='line' id='top-line'></div>
-                    <Link to="/is" className='icon-link'>
-                        <img src={instaIcon} alt='instagram' />
-                    </Link>
-                    <Link to="/is" className='icon-link'>
-                        <img src={ttIcon} alt='tiktok' />
-                    </Link>
-                    <Link to="/is" className='icon-link'>
-                        <img src={fbIcon} alt='facebook' />
-                    </Link>
-
-                    <div className='line' id='bottom-line'></div>
-                </div>
                 <div id='content-container'>
-                    <div id='main-content'>
-
-                    </div>
-                    <div id='bottom-content'>
-                        <div id='bottom-panel-wrapper'>
-                            <div id='bottom-panel'>
-                                <div className='bottom-panel-item' id='bottom-panel-destination'>
-                                    <img src={destIcon} alt='destination' />
-                                    <TextField label={t('destination')} />
-                                </div>
-                                <div className='bottom-panel-divider'></div>
-                                <div className='bottom-panel-item' id='bottom-panel-dates'>
-
-                                </div>
-                                <div className='bottom-panel-divider'></div>
-                                <div className='bottom-panel-item' id='bottom-panel-group'>
-                                    {t('ueee')}
-                                </div>
-                            </div>
-                        </div>                
+                    <div className='floor-container' id='first-container'>
+                        
+                        <div id='main-caption'>
+                            {t('mainCaption')}
+                        </div> 
                     </div>
                     
                 </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import "../../styles/navbar/navbar.scss";
 import appLogo from "../../assets/icons/navbar/LOGO.svg";
-import languageSelectorIcon from "../../assets/icons/navbar/language_idle.svg";
-import favouritesIcon from "../../assets/icons/navbar/favourites_idle.svg";
-import profileLinkIcon from "../../assets/icons/navbar/profile_idle.svg";
+import languageSelectorIcon from "../../assets/icons/navbar/languages.svg";
+import favouritesIcon from "../../assets/icons/navbar/favourites.svg";
+import profileLinkIcon from "../../assets/icons/navbar/profile.svg";
 import { FocusEvent, useState } from "react";
 import { useTranslation } from "react-i18next";
 import i18next from 'i18next';
@@ -11,20 +11,12 @@ import i18next from 'i18next';
 export default function Navbar() {
     const { t } = useTranslation();
     const resources = i18next.options.resources as Record<string, any>;
-    console.log(i18next.options.resources);
     const lngs = Object.keys(resources).map(code => {
       return {
         code: code,
         name: resources[code].translation.nativeName
       };
     });
-
-    console.log(Object.keys(resources).map(code => {
-      return {
-        code: code,
-        name: resources[code].translation.nativeName
-      };
-    }));
 
     const [focused, setFocused] = useState(false);
 
@@ -40,39 +32,48 @@ export default function Navbar() {
 
     return (
       <>
-        <nav id="navbar"> 
-            <Link to="/">
-              <img src={appLogo} alt="home logo" />
-            </Link>
-            <div className="line"></div>
-            <Link to="/housing-offer" className="navbar-text">
-              {t('createHousing')}
-            </Link>
-            <div 
-              tabIndex={0}
-              className="navbar-item navbar-icon"
-              onFocus={handleFocus}
-              onBlur={handleBlur}>
-              <img src={languageSelectorIcon} alt="languages" />
-              <div className={`language-menu  ${focused ? 'active' : ''}`}>
-              <select
-                value={i18next.resolvedLanguage}
-                onChange={(e) => i18next.changeLanguage(e.target.value)}
-              >
-                {lngs.map((lng) => (
-                  <option key={lng.code} value={lng.code}>
-                    {lng.name}
-                  </option>
-                ))}
-              </select>
+
+        <nav id="navbar-wrapper">
+          <div id="navbar-border">
+            <div id="navbar">
+              
+              
+              <Link to="/housing-offer" className="navbar-text">
+                {t('createHousing')}
+              </Link>
+              <Link to="/" id="home-logo">
+                <img src={appLogo} alt="home logo" />
+              </Link>
+              <div id="navbar-icons-group">
+                <div 
+                  tabIndex={0}
+                  className="navbar-item navbar-icon"
+                  onFocus={handleFocus}
+                  onBlur={handleBlur}>
+                  <img src={languageSelectorIcon} alt="languages" className="inner-icon" />
+                  <div className={`language-menu  ${focused ? 'active' : ''}`}>
+                    <select
+                      value={i18next.resolvedLanguage}
+                      onChange={(e) => i18next.changeLanguage(e.target.value)}>
+                      {lngs.map((lng) => (
+                        <option key={lng.code} value={lng.code}>
+                          {lng.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+                <Link className="navbar-icon" to="/favourites">
+                  <img src={favouritesIcon} alt="favourites"  className="inner-icon"/>
+                </Link>
+                <Link className="navbar-icon" to="/profile/my">
+                  <img src={profileLinkIcon} alt="profile" className="inner-icon"/>
+                </Link> 
               </div>
-            </div>
-            <Link className="navbar-icon" to="/favourites">
-              <img src={favouritesIcon} alt="favourites" />
-            </Link>
-            <Link className="navbar-icon" to="/profile/my">
-              <img src={profileLinkIcon} alt="profile" />
-            </Link>
+               
+            </div> 
+          </div>
+                  
         </nav>
       </>  
     );
