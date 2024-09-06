d14de96aecc8d37b83b3dd99d8d1598481493d21
Sagof1 <valerakargap@gmail.com>
2024-09-06 23:24:09 +0200

    File saving WIP

--- src/components/housing/sections/FeaturesSection.tsx
+++ src/components/housing/sections/FeaturesSection.tsx
@@ -1,8 +1,9 @@
 import { useTranslation } from "react-i18next";
-import { Housing } from "../HousingPage";
+import { Feature, Housing } from "../HousingPage";
 
 import '../../../styles/housing/sections/features-section.scss';
 import { useState } from "react";
+import FeatureService from "../../../helpers/FeatureService";
 
 interface FeaturesSectionProps {
     housingData: Housing;
@@ -13,6 +14,9 @@ export default function FeaturesSection({ housingData }: FeaturesSectionProps) {
 
     const [isExpanded, setIsExpanded] = useState(false);
 
+    const groupedFeatures = groupByProperty(housingData.features, "typeCode");
+    const [groupA1, groupA2] = splitGroupsEqually(groupedFeatures);
+
     return (
         <>
             <section className="features-section">
@@ -20,9 +24,68 @@ export default function FeaturesSection({ housingData }: FeaturesSectionProps) {
                     {t('HousingPage.Sections.Features.Header')}
                 </h2>
                 <div className="features-columns">
-                    <div className="features-columns__col-1">
+                    <div className="features-columns__col">
+                        {Object.entries(groupA1).map(([type, feature], index) => {
+                            return (
+                                <>
+                                    <div className="features-block" key={index}>
+                                        <h3 className="features-type">
+                                            {t(type)}
+                                        </h3>
+                                        {feature.map((f, index) => {
+                                            return (
+                                                <>
+                                                    <div className="feature" key={index}>
+                                                        <div className="feature__icon">
+                                                            <img src={FeatureService.getFeatureIcon(f.featureIcon)} />
+                                                        </div>
+                                                        
+                                                        <div className="feature__data">
+                                                            <div className="feature__name">{t(f.nameCode)}</div>
+                                                            {f.descriptionCode ? (
+                                                                <div className="feature__description">{t(f.descriptionCode)}</div>
+                                                            ) : ''}
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </>
+                                            );
+                                        })}
+                                    </div>
+                                </>
+                            );
+                        })}
                     </div>
-                    <div className="features-columns__col-2">
+                    <div className="features-columns__col">
+                        {Object.entries(groupA2).map(([type, feature], index) => {
+                            return (
+                                <>
+                                    <div className="features-block" key={index}>
+                                        <h3 className="features-type">
+                                            {t(type)}
+                                        </h3>
+                                        {feature.map((f, index) => {
+                                            return (
+                                                <>
+                                                    <div className="feature" key={index}>
+                                                        <div className="feature__icon">
+                                                            <img src={FeatureService.getFeatureIcon(f.featureIcon)} />
+                                                        </div>
+                                                        <div className="feature__data">
+                                                            <div className="feature__name">{t(f.nameCode)}</div>
+                                                            {f.descriptionCode ? (
+                                                                <div className="feature__description">{t(f.descriptionCode)}</div>
+                                                            ) : ''}
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </>
+                                            );
+                                        })}
+                                    </div>
+                                </>
+                            );
+                        })}
                     </div>
                 </div>
                 <button className="show-more-btn" onClick={() => setIsExpanded(!isExpanded)}>
@@ -32,3 +95,64 @@ export default function FeaturesSection({ housingData }: FeaturesSectionProps) {
         </>
     );
 }
+
+type GroupedRecord = Record<string, Feature[]>;
+
+function groupByProperty(items: Feature[], key: keyof Feature): GroupedRecord {
+    return items.reduce((acc, item) => {
+      const groupKey = item[key] as string;
+      if (!acc[groupKey]) {
+        acc[groupKey] = [];
+      }
+      acc[groupKey].push(item);
+      return acc;
+    }, {} as GroupedRecord);
+}
+
+function splitGroupsEqually(grouped: GroupedRecord): [GroupedRecord, GroupedRecord] {
+    const keys = Object.keys(grouped);
+    const halfLength = Math.ceil(keys.length / 2);
+  
+    const group1: GroupedRecord = {};
+    const group2: GroupedRecord = {};
+  
+    keys.forEach((key, index) => {
+      if (index < halfLength) {
+        group1[key] = grouped[key];
+      } else {
+        group2[key] = grouped[key];
+      }
+    });
+  
+    return [group1, group2];
+}
+
+function splitGroupsWithLimit(grouped: GroupedRecord, limit: number): [GroupedRecord, GroupedRecord] {
+    const group1: GroupedRecord = {};
+    const group2: GroupedRecord = {};
+  
+    let totalInGroup1 = 0;
+    let totalInGroup2 = 0;
+  
+    Object.keys(grouped).forEach((key) => {
+      const groupSize = grouped[key].length;
+  
+      if (totalInGroup1 + groupSize <= limit) {
+        group1[key] = grouped[key];
+        totalInGroup1 += groupSize;
+      } else if (totalInGroup2 + groupSize <= limit) {
+        group2[key] = grouped[key];
+        totalInGroup2 += groupSize;
+      } else {
+        // If the group is too big to fit into one of the splits, break it down
+        const splitAt = limit - totalInGroup2;
+        group2[key] = grouped[key].slice(0, splitAt);
+        group1[key] = grouped[key].slice(splitAt);
+  
+        totalInGroup1 += group1[key].length;
+        totalInGroup2 += group2[key].length;
+      }
+    });
+  
+    return [group1, group2];
+  }
