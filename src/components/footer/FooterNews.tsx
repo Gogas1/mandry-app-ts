3fe673cd04236b7c5c6e69bc4e22d748610b441c
Sagof1 <valerakargap@gmail.com>
2024-09-17 20:28:29 +0200

    Footer header r

---
+++ src/components/footer/FooterNews.tsx
@@ -0,0 +1,48 @@
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+import FooterSection from '../home/FooterSection';
+import NotificationSearch from '../account/profile/notifications/NotificationSearch';
+import FooterPromotionsSection from '../footer/FooterPromotionSection';
+import FooterHeader from './FooterHeader';
+
+import '../../styles/pages/footer/footer-news.scss'
+
+export default function NewsPage() {
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+
+    const [selectedPromHotel, setSelectedPromHotel] = useState<string>('base');
+    
+
+    const handleSelectPromHotel = (value: string) => {
+        setSelectedPromHotel(value);
+    }
+
+    const renderPromSection = () => {
+        switch (selectedPromHotel) {
+            case 'base':
+                return <FooterPromotionsSection handlePromotion={handleSelectPromHotel}/>;
+            case '0':
+                return <FooterPromotionsSection handlePromotion={handleSelectPromHotel}/>;
+            case '1':
+                return <FooterPromotionsSection handlePromotion={handleSelectPromHotel}/>;
+            case '2':
+                return <FooterPromotionsSection handlePromotion={handleSelectPromHotel}/>;
+            default:
+                return <FooterPromotionsSection handlePromotion={handleSelectPromHotel}/>;
+        }
+    }
+
+    return (
+        <div className='news-page'>
+            <div className='content-container'>
+                <FooterHeader title='MainPage.Sections.Footer.Pages.News.Header'/>
+                    <div className='promotion-section'>{renderPromSection()}</div>
+                <div className='divider' />
+            </div>
+            <FooterSection />
+        </div>
+    )
+}
